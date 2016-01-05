b9feb7c8ae78888b78f627a574e1f61c1f6181e3
Bryan Miller <gratiafide@yahoo.com>
2016-01-04 20:07:06 -0600

    finished form and relocated it to a partial

---
+++ resources/views/flyers/_form.blade.php
@@ -0,0 +1,48 @@
+<div class="form-group">
+    <label for="street">Street:</label>
+    <input type="text" name="street" class="form-control" value="">
+</div>
+<div class="form-group">
+    <label for="city">City:</label>
+    <input type="text" name="city" class="form-control" value="">
+</div>
+<div class="form-group">
+    <label for="zip">Zip Code:</label>
+    <input type="text" name="zip" class="form-control" value="">
+</div>
+<div class="form-group">
+    <label for="country">Country:</label>
+    <select name="country" class="form-control">
+        @foreach($countries::all() as $country => $code)
+            <option value="{{$code}}">{{$country}}</option>
+        @endforeach
+    </select>
+</div>
+<div class="form-group">
+    <label for="state">State:</label>
+    <input type="text" name="state" class="form-control" value="">
+</div>
+
+<hr>
+
+<div class="form-group">
+    <label for="price">Price:</label>
+    <input type="text" name="price" class="form-control" value="">
+</div>
+
+
+<div class="form-group">
+    <label for="description">Home Description:</label>
+    <textarea name="description" class="form-control" rows="10"></textarea>
+</div>
+
+<div class="form-group">
+    <label for="photos">Photos:</label>
+    <input type="file" name="photos" class="form-control">
+</div>
+
+<hr>
+
+<div class="form-group">
+    <button type="submit" class="btn btn-primary">Submit Flyer</button>
+</div>
