000de2a348a320165ea0ce3fa289487ffbeb81ac
Bryan Miller <gratiafide@yahoo.com>
2016-01-08 00:07:57 -0600

    Added DropzoneJS demo to create flyer page

--- resources/views/flyers/create.blade.php
+++ resources/views/flyers/create.blade.php
@@ -6,6 +6,8 @@
 
     <form method="POST" action="/flyers" enctype="multipart/form-data" class="col-md-6">
         @include('flyers._form')
+
+
         @if(count($errors) > 0)
             <div class="alert alert-danger">
                 <ul>
@@ -14,6 +16,17 @@
                     @endforeach
                 </ul>
             </div>
-            @endif
-    </form>
+        @endif
+    </form><hr>
+    </div>
+    <div class="container">
+        <h1>Upload pictures of your house!</h1>
+        <form action="/flyers" method="POST" class="dropzone">
+            {{csrf_field()}}
+        </form>
+    </div>
+@stop
+
+@section('scripts.footer')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.2.0/dropzone.js"></script>
 @stop
--- resources/views/static-pages/home.blade.php
+++ resources/views/static-pages/home.blade.php
@@ -3,7 +3,8 @@
 @section('content')
     <div class="jumbotron">
         <h1>Project Flyer</h1>
-        <p>This is a template showcasing the optional theme stylesheet included in Bootstrap. Use it as a starting point to create something more unique by building on or modifying it.</p>
+        <p>If you'd like to create a cloud-based flyer for you to sell your home, then
+        you've come to the right place!  Click "Create a Flyer" below or "Create a home listing" above to get started.</p>
         <a href="/flyers/create" class="btn btn-primary">Create a Flyer</a>
     </div>
 @stop
