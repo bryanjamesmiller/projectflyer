c869bfb24fb53279abec085f457bdea5da5df89a
vagrant <vagrant@homestead>
2016-01-20 00:06:38 +0000

    fixing config

--- config/app.php
+++ config/app.php
@@ -155,7 +155,6 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class
     ],
 
     /*
@@ -201,7 +200,6 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        'Image'     => Intervention\Image\Facades\Image::class
 
     ],
 
