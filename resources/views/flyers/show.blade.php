dbf79de91936398b052de1438b3add1049fc18f0
Bryan Miller <bmiller100wpm@gmail.com>
2016-01-20 23:01:44 +0000

    fixed thumbnail margins

--- resources/views/flyers/show.blade.php
+++ resources/views/flyers/show.blade.php
@@ -12,31 +12,39 @@
             <div class="description">{!! nl2br($flyer->description) !!}</div>
         </div>
 
-        <div class="col-md-9">
-            @foreach($flyer->photos as $photo)
-                <img src="{{$photo->path}}" alt="">
+        <div class="col-md-8 gallery">
+            @foreach($flyer->photos->chunk(4) as $set)
+                <div class="row">
+                    @foreach($set as $photo)
+                        <div class="col-md-3 gallery_image">
+                            <a href="/{{ $photo->path }}">
+                                <img src="/{{ $photo->thumbnail_path }}" alt="">
+                            </a>
+                        </div>
+                    @endforeach
+                </div>
             @endforeach
         </div>
     </div>
 
-    <hr>
+        <hr>
 
-    <form id="addPhotosForm"
-          action="{{route('store_photo_path', [$flyer->zip, $flyer->street])}}"
-          method="POST"
-          class="dropzone">
-        {{csrf_field()}}
-    </form>
+        <form id="addPhotosForm"
+              action="{{route('store_photo_path', [$flyer->zip, $flyer->street])}}"
+              method="POST"
+              class="dropzone">
+            {{csrf_field()}}
+        </form>
 
-@stop
+        @stop
 
-@section('scripts.footer')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.2.0/dropzone.js"></script>
-    <script>
-        Dropzone.options.addPhotosForm = {
-            paramName: 'photo',
-            maxFileSize: 3,
-            acceptedFiles: '.jpg, .jpeg, .png, .bmp'
-        };
-    </script>
+        @section('scripts.footer')
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.2.0/dropzone.js"></script>
+            <script>
+                Dropzone.options.addPhotosForm = {
+                    paramName: 'photo',
+                    maxFileSize: 3,
+                    acceptedFiles: '.jpg, .jpeg, .png, .bmp'
+                };
+            </script>
 @stop
