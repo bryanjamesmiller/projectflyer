aab381bf9f57820242e82f8a1901ef4c31eabf60
Bryan Miller <bmiller100wpm@gmail.com>
2016-01-23 23:49:20 +0000

    best yet

--- resources/views/button.blade.php
+++ resources/views/button.blade.php
@@ -10,6 +10,19 @@
     <div class="row">
         <div class="col-xs-12">
             <span id="liked_btn"><button class="btn btn-default vote_btn"><span class="number">{{$total_likes = 7}}</span>&nbsp;Likes</button></span>
+        </div>
+    </div>
+
+    <div class="jumbotron">
+        <h1>Project Flyer</h1>
+        <p>If you'd like to create a cloud-based flyer for you to sell your home, then
+            you've come to the right place!  Click "Create a Flyer" below or "Create a home listing" above to get started.</p>
+        <a href="/flyers/create" class="btn btn-primary">Create a Flyer</a>
+    </div>
+    <div class="row">
+        <div class="col-xs-12">
+            <span id="liked_btn"><button class="btn btn-default vote_btn"><span class="number">{{$total_likes = 7}}</span>&nbsp;Likes</button></span>
+        </div>
     </div>
 @stop
 
@@ -20,9 +33,7 @@
             $('.vote_btn').click(function(e) {
                 var url = "http://clothing.app/updateProductOption";
                 var $post = {};
-console.log($(this).find(".number").text());
-                console.log($(".number").text());
-                var newNum = parseInt($(".number").text()) + 1;
+                var newNum = parseInt($(this).find(".number").text()) + 1;
 
                 console.log(typeof newNum);
                 console.log(newNum);
