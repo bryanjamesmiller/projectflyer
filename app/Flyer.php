5b069f6b4d7ecde3fac28cba78243d5670396253
Bryan Miller <bmiller100wpm@gmail.com>
2016-02-09 01:12:35 +0000

    okok

--- app/Flyer.php
+++ app/Flyer.php
@@ -35,7 +35,7 @@ class Flyer extends Model
      * @return mixed
      */
     public static function locatedAt($zip, $street){
-//Test for pull merge github practiceccccccccccccccccccccccc
+//Test for pull merge github azzzzzzzzzzzzzzzzzz
         //$street = str_replace('-', ' ', $street);
         return static::where(compact('zip'))->firstOrFail();
     }
